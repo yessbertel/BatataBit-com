ecfe4a9033e7f08293dd530307a2e4e72c51d635
yessbertel <ybertel0307@gmail.com>
2025-11-26 01:15:12 -0500

    Fix components

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,6 @@
 // import "@/app/globals.css";
-import Header from "@/components/Header";
-import Footer from "@/components/Footer";
+import Header from "@/components/layout/Header";
+import Footer from "@/components/layout/Footer";
 import Head from "next/head";
 
 const Layout = ({ children }) => {
